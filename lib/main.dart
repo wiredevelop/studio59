24baff1d08972a92ff6ef425d016a16d731f5cea
root <root@srv1.wiredevelop.pt>
2026-03-20 10:44:54 +0000

    Update app icons

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
